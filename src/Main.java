6bb469cbd0f10ba10064c22b8f1b4f0ad6bc3c30
MarcKlesiewicz <marc247hedu.easj.dk>
2019-12-20 09:07:11 +0100

    Opgave 1 og 2 done

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,32 @@
-/**
- * Developed by András Ács (acsandras@gmail.com)
- * Zealand / www.zealand.dk
- * Licensed under the MIT License
- * 13/12/2019
- */
+import org.w3c.dom.ls.LSOutput;
 
 public class Main {
 
-    // TODO Din kode her
 
-    public String hello(String navn) {
-        return "";
+    public static void main(String[] args) {
+
+        //Opgave 1.a
+        System.out.println("Hello World!");
+
+        //Opgave 1.b
+        System.out.printf(hello("András"));
+
+        System.out.println(hello(""));
+
+
     }
+
+
+    public static String hello(String navn) {
+
+        if (navn == "András") {
+            return "Hello " + navn+"!";
+        }else{
+            return "Hello World!";
+        }
+
+    }
+
+
 
 }
